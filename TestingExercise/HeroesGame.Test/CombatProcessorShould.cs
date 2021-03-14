621e6b19614a1eea912f3c187d45079963557231
IT-Carrer <youremail@mail.com>
2021-03-14 12:06:55 +0200

    Initial commit

---
+++ TestingExercise/HeroesGame.Test/CombatProcessorShould.cs
@@ -0,0 +1,72 @@
+﻿using HeroesGame.Constant;
+using HeroesGame.Contract;
+using HeroesGame.Implementation.Hero;
+using HeroesGame.Implementation.Monster;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesGame.Test
+{
+    class CombatProcessorShould
+    {
+        private CombatProcessor cp;
+
+        [SetUp]
+        public void Setup()
+        {
+            cp = new CombatProcessor(new Hunter());
+        }
+
+        [Test]
+        public void InitializeCorrectly()
+        {
+            //Assert 
+            Assert.That(cp.Hero, Is.Not.Null);
+            Assert.That(cp.Logger, Is.Not.Null);
+            Assert.That(cp.Logger, Is.Empty);
+        }
+
+        private void LevelUpHero(int levels)
+        {
+            for (int i = 0; i < levels; i++)
+            {
+                cp.Hero.GainExperience(HeroConstants.MaximumExperience);
+            }
+        }
+
+        [Test]
+        public void FightCorrectly_WeakerEnemy()
+        {
+            //Arrange
+            IMonster monster = new MedusaTheGorgon(level: 1);
+            LevelUpHero(50);
+
+            //Act
+            cp.Fight(monster);
+            List<string> logger = cp.Logger;
+
+            //Assert - expected monster to die in 1 hit
+            Assert.That(logger.Count, Is.EqualTo(2));
+            Assert.That(logger, Does.Contain("The Hunter hits the MedusaTheGorgon dealing 510 damage to it.")
+                .And.Contains("The monster dies. (4 XP gained.)"));
+        }
+
+        [Test]
+        public void FightCorrectly_StrongerEnemy()
+        {
+            //Arrange
+            IMonster monster = new MedusaTheGorgon(level: 50);
+
+            //Act
+            cp.Fight(monster);
+            List<string> logger = cp.Logger;
+
+            //Assert - expected hero to die in 1 hit but heal 3 times which gives him an extra turn
+            Assert.That(logger, Has.Count.EqualTo(12));
+            Assert.That(logger, Does.Contain("The hero dies on level 1 after 4 fights."));
+        }
+
+    }
+}
---
+++ TestingExercise/HeroesGame.Test/HeroShould.cs
@@ -0,0 +1,178 @@
+﻿using HeroesGame.Constant;
+using HeroesGame.Contract;
+using HeroesGame.Implementation.Hero;
+using NUnit.Framework;
+using System;
+using Moq;
+using Moq.Protected;
+
+namespace HeroesGame.Test
+{
+    public class HeroShould
+    {
+        private Mock<Mage> hero;
+
+        [SetUp]
+        public void Setup()
+        {
+            hero = new Mock<Mage>();
+            hero.Protected().Setup("LevelUp").CallBase();
+        }
+
+        [Test]
+        public void HaveCorrectInitialValues()
+        {
+            //Assert 
+            Assert.That(hero.Object.Level, Is.EqualTo(HeroConstants.InitialLevel));
+            Assert.That(hero.Object.Experience, Is.EqualTo(HeroConstants.InitialExperience));
+            Assert.That(hero.Object.MaxHealth, Is.EqualTo(HeroConstants.InitialMaxHealth));
+            Assert.That(hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth));
+            Assert.That(hero.Object.Armor, Is.EqualTo(HeroConstants.InitialArmor));
+            Assert.That(hero.Object.Weapon, Is.Not.Null);
+        }
+
+        [Test]
+        public void TakeHitCorrectly_Range([Range(from: 70, to: 100, step: 10)] int damage)
+        {
+            //Act
+            hero.Object.TakeHit(damage);
+
+            //Assert
+            Assert.That(hero.Object.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        }
+
+        //[Test]
+        //public void TakeHitCorrectly()
+        //{
+        //    hero = new Warrior();
+
+        //    //Act
+        //    int damage = 50;
+        //    hero.TakeHit(50);
+
+        //    //Assert
+        //    Assert.That(hero.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        //}
+
+        //[Test]
+        //public void TakeHitCorrectly_Combinatorial([Values(40, 50, 60)] int damage)
+        //{
+        //    //Act
+        //    hero.TakeHit(damage);
+
+        //    //Assert
+        //    Assert.That(hero.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        //}
+
+        //[Test]
+        //[TestCase(arg: -10)]
+        //[TestCase(arg: -20)]
+        //[TestCase(arg: -30)]
+        //public void ThrowExceptionForNegativeTakeHitValue(int damage)
+        //{
+        //    //Assert
+        //    Assert.Throws<ArgumentException>(() => hero.TakeHit(damage), "Damage value cannot be negative");
+        //}
+
+        //[Test]
+        //[TestCase(arg: 10)]
+        //[TestCase(arg: 20)]
+        //[TestCase(arg: 30)]
+        //public void TakeHitCorrectly_TestCase(int damage)
+        //{
+        //    //Act
+        //    hero.TakeHit(damage);
+
+        //    //Assert
+        //    Assert.That(hero.Health, Is.EqualTo(HeroConstants.InitialMaxHealth - damage + HeroConstants.InitialArmor));
+        //}
+
+        //[Test]
+        //public void ThrowExceptionForNegativeTakeHitValue()
+        //{
+        //    //Act 
+        //    var damage = -50;
+
+        //    //Assert
+        //    Assert.Throws<ArgumentException>(() => hero.TakeHit(damage), "Damage value cannot be negative");
+        //}
+
+        //[Test]
+        //public void GainExperienceCorrectly([Range(from: 25, to: 500, step: 25)] double xp)
+        //{
+        //    //Act
+        //    hero.GainExperience(xp);
+
+        //    //Assert
+        //    if (xp >= HeroConstants.MaximumExperience)
+        //    {
+        //        double expectedXp = (HeroConstants.InitialExperience + xp) % HeroConstants.MaximumExperience;
+
+        //        Assert.That(hero.Experience, Is.EqualTo(expectedXp));
+        //        Assert.That(hero.Level, Is.EqualTo(HeroConstants.InitialLevel + 1));
+        //    }
+        //    else
+        //    {
+        //        Assert.That(hero.Experience, Is.EqualTo(HeroConstants.InitialExperience + xp));
+        //    }
+        //}
+
+        //[Test]
+        //public void HealCorrectly([Range(from: 5, to: 25, step: 1)] int level, [Range(from: 25, to: 500, step: 25)] int damage)
+        //{
+        //    //Act
+        //    //level up our hero
+        //    LevelUp(level);
+        //    //then take a hit
+        //    double totalDamage = HeroConstants.InitialMaxHealth + damage;
+        //    totalDamage = hero.TakeHit(totalDamage);
+        //    hero.Heal();
+
+        //    //Assert
+        //    int healValue = hero.Level * HeroConstants.HealPerLevel;
+        //    double expectedHealth = (hero.MaxHealth - totalDamage) + healValue;
+
+        //    if (expectedHealth > hero.MaxHealth)
+        //    {
+        //        expectedHealth = hero.MaxHealth;
+        //    }
+
+        //    Assert.That(hero.Health, Is.EqualTo(expectedHealth));
+        //}
+
+        //private void LevelUp(int levels)
+        //{
+        //    for (int i = 0; i < levels; i++)
+        //    {
+        //        hero.GainExperience(HeroConstants.MaximumExperience);
+        //    }
+        //}
+        //[Test]
+        //public void NotBeBornDead()
+        //{
+        //    //Act
+        //    bool isDead = hero.IsDead();
+
+        //    //Assert
+        //    Assert.That(hero.IsDead, Is.False);
+        //}
+
+        //[Test]
+        //public void BeDeadWhenCriticallyHit([Range(from: 50, to: 100, step: 25)] double damage)
+        //{
+        //    //Act
+        //    damage = hero.TakeHit(damage);
+
+        //    //Assert
+        //    if (damage >= hero.MaxHealth)
+        //    {
+        //        Assert.That(hero.IsDead);
+        //    }
+        //    else
+        //    {
+        //        Assert.That(hero.IsDead, Is.False);
+        //    }
+        //}
+
+    }
+}
